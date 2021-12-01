f7e9a351f0f98b0ffe05298afb37ddee5406b968
DanielLorenzo22 <daniellorenzocabrera@gmail.com>
2021-12-01 11:29:40 +0000

    Implementando mostrar info peli

--- clases/Videoclub.php
+++ clases/Videoclub.php
@@ -4,8 +4,6 @@
     ENLACE PARA CONSULTAR INFORMACION DE UNA PELICULA
     https://api.themoviedb.org/3/movie/512195?api_key=0bd8a8eb5c3993fd33872f75979674ff&language=es
 
-
-
     cURL -< Client URL
     curl_init() -> Inicializa una nueva sesión y devuelve un objecto cURL handle para usar en las funciones curl_setopt(), curl_exec(), and curl_close().
     curl_setopt($handle,int $option, value) -> Establece las opciones de la conexion curl (int son variables ya creadas en php)
@@ -21,20 +19,19 @@
 */
 
     class Videoclub{
-        private $api_key;
+        private $api_key = "0bd8a8eb5c3993fd33872f75979674ff";
         private $url;
-        public $resultados;
+        private $resultados;
         private $curlHandle;
 
 
-        public function __construct($url){
-            $this->url = $url;
-            $this->curlHandle = curl_init($url); 
-            //curl_setopt($this->curlHandle, CURLOPT_URL, $url);
+        public function __construct(){
+
+            $this->url = "https://api.themoviedb.org/3/discover/movie?api_key={$this->api_key}&language=es";
+            $this->curlHandle = curl_init($this->url); 
             curl_setopt($this->curlHandle, CURLOPT_RETURNTRANSFER, true);
             $datos = json_decode(curl_exec($this->curlHandle), true);
             $this->resultados = $datos["results"];
-            //var_dump($this->resultados);
         }
 
 
@@ -49,22 +46,20 @@
             }
         }
 
-        public function mostrarInformacionPelicula($url_pelicula){
-            $peticion = curl_init($url_pelicula);
-            curl_setopt($peticion, CURLOPT_RETURNTRANSFER, true);
-            $datos = json_decode(curl_exec($peticion), true);
-            
+        public function mostrarInformacionPelicula($id_pelicula){
+            $url_pelicula = "https://api.themoviedb.org/3/movie/{$id_pelicula}?api_key={$this->api_key}&language=es";
+            $url_actores = "https://api.themoviedb.org/3/movie/{$id_pelicula}/credits?api_key={$this->api_key}&language=en-US";
+            $datos = $this->hacerPeticion($url_pelicula);
+            $actores = $this->hacerPeticion($url_actores);
             echo "<div class='film-info'>";
             echo "<figure><img src='https://image.tmdb.org/t/p/w185{$datos['poster_path']}'></figure>";
             echo "<h2>{$datos['original_title']}</h2>";
             echo "<p class='descripcion'>{$datos['overview']}</p>";
             echo "<article class='generos'>";
-            foreach($datos['genres'] as $genero){
-                echo $genero['name'];
-            }
+            $this->mostrarElementos($datos['genres'],"name");
             echo "</article>";
             echo "<article class='actores'>";
-            foreach($datos[])
+            $this->mostrarElementos($actores['cast'],"name");
             echo "</article>";
             echo "</div>";
 
@@ -84,6 +79,26 @@
 
 
     }
+
+    public function getApiKey(){
+        return $this->api_key;
+    }
+
+    private function mostrarElementos($array,$key){
+        foreach($array as $element){
+            echo "<p>{$element[$key]}</p>";
+        }
+    }
+
+
+    private function hacerPeticion($url){
+        $peticion = curl_init($url);
+        curl_setopt($peticion, CURLOPT_RETURNTRANSFER, true);
+        $datos = json_decode(curl_exec($peticion), true);
+        curl_close($peticion);
+        return $datos;
+    }
+
 
     }
 
--- controlador.php
+++ controlador.php
@@ -4,17 +4,15 @@ spl_autoload_register(function($nombre_clase){
     include "clases/" . $nombre_clase . ".php";
 });
 
-$url_peliculas = "https://api.themoviedb.org/3/discover/movie?api_key=0bd8a8eb5c3993fd33872f75979674ff&language=es";
-$VIDEOCLUB =  new Videoclub($url_peliculas);
+
+$VIDEOCLUB =  new Videoclub();
 
 if(empty($_GET)){
     $VIDEOCLUB->mostrarPeliculas();
 }else if(isset($_GET['id'])){
 
     $id_pelicula = $_GET['id'];
-    $url_pelicula = "https://api.themoviedb.org/3/movie/{$id_pelicula}?api_key=0bd8a8eb5c3993fd33872f75979674ff&language=es";
-
-    $VIDEOCLUB->mostrarInformacionPelicula($url_pelicula);
+    $VIDEOCLUB->mostrarInformacionPelicula($id_pelicula);
 }
 
 
